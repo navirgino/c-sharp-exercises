017e08865ee05eda8dd7fa87cfd614f10ed0ee7e
Nico Virgino <nicovirgino@gmail.com>
2019-12-24 12:22:24 -0600

    made some mathematical operators

---
+++ c-sharp-exercises/c-sharp-exercises/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace c_sharp_exercises
+{
+    class Program
+    {
+        //sum of two numbers
+        public int SumOf(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int QuotOf(int a, int b)
+        {
+            return a / b;
+        }
+
+        public double ProdOf(double a, double b)
+        {
+            return a * b;
+        }
+
+        static void Main(string[] args)
+        {
+            Program e = new Program();
+
+            int sum = e.SumOf(2, 3);
+            int quotient = e.QuotOf(18, 3);
+            double product = Math.Round(e.ProdOf(14.0, 2.2),1);
+
+
+            Console.WriteLine(sum);
+            Console.WriteLine(quotient);
+            Console.WriteLine(product);
+
+
+        }
+     
+    }
+}
